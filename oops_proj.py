e9cebc88cad620cc9b655b1955b88094c0bec794
gandlanandhu <gandlarameshpatel@gmail.com>
2025-08-28 18:00:10 +0530

    signup and signin functionality added successfully

--- oops_proj.py
+++ oops_proj.py
@@ -13,13 +13,39 @@ class chatbot:
                          4.press 4 to message a friend
                          5.press any key to exit""")
         if user_input=='1':
-            pass
+            self.signup()
         elif user_input=='2':
-            pass
+            self.signin()
         elif user_input=='3':
             pass
         elif user_input=='4':
             pass
         else:
             exit()
+    
+    def signup(self):
+        email=input("please enter your mail:")
+        pwd=input("please enter your password:")
+        self.username=email
+        self.password=pwd
+        print("you've successfully loggedin")
+        print("\n")
+        self.menu()
+
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("please press 1 in main menu to signup first")
+        else:
+            uname=input("please enter your mail:")
+            pwd=input("please enter your password:")
+            if self.username==uname and self.password==pwd:
+                print("you've signed in successfully")
+                self.loggedin=True
+            else:
+                print("please enter correct credentials")
+
+        print("\n")
+        self.menu()
+
+    
 obj=chatbot()
