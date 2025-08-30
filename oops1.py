0d3dd3f307f80f06343b83e3c457f361a4194226
gandlanandhu <gandlarameshpatel@gmail.com>
2025-08-30 14:33:49 +0530

    encapsulation getter stter static method

--- oops1.py
+++ oops1.py
@@ -1,18 +1,37 @@
 #initiate a class
 class employee:
+    #static variable
+    _user_id=0
+
     #this is constructor-called special function/dunder method/magic method
     def __init__(self):
         #these are the data/attributes of the class
+        self.__uid=employee._user_id
+        employee._user_id+=1
+        self.__rollno=1234
         self.name="nandhu"
         self.id=123
         self.designation="sde"
+    #getter
+    def get_rollno(self):
+        return self.__rollno
+    #setter
+    def set_rollno(self,value):
+        self.__rollno=value
+
+    #static method
+    def get_uid():
+        return employee._user_id
 
     #now lets create an function ie method(function in class is a method)
     def travel(self,destination):
         print(f"now the employee is travelling to {destination}")
 #creating an object
 nandhu=employee()
-print(nandhu.id)
-print(nandhu)
+#print(nandhu.id)
+#print(nandhu)
 #calling a method
-nandhu.travel("telangana")
+#nandhu.travel("telangana")
+#creating an attribute outside the class
+nandhu.name="ram"
+#print(nandhu.name)
--- trash.py
+++ trash.py
@@ -1,2 +1,14 @@
-from oops_proj import chatbot
-obj=chatbot()
+#from oops_proj import chatbot
+#obj=chatbot()
+from oops1 import employee
+obj1=employee()
+#print(obj1._employee__rollno)
+#first assign object and then the following(for getter and setter)
+#print(obj1.get_rollno())
+#obj1.set_rollno(903)
+#print(obj1.get_rollno())
+#program for class variable or static variable
+#print(obj1.uid)
+#obj2=employee()
+#print(obj2.uid)
+print(employee.get_uid())
